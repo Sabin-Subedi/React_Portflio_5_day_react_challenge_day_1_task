4616df31f9ae631aba028abedede48501b4c2c34
Sabin Subedi <sabinsubedi125@gmail.com>
2020-09-08 11:31:08 +0545

    Build added

---
+++ build/precache-manifest.deb6251fdf49b319293d2ac531df89cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f664ed78286a1d1890a447f69578d50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62eaaf11104c409b2ebb",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62eaaf11104c409b2ebb",
+    "url": "/static/js/main.78ece2c2.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
